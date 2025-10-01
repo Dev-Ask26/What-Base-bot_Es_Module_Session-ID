de3928250694402899d04fee928629a967b6cd4c
Dev Ask <techdevask@gmail.com>
2025-10-01 19:01:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 // ==================== config.js ====================
 export default {
-  PREFIX: "!",                       // Préfixe des commandes
-  OWNER_NUMBER: "241xxxxxxxx",       // Numéro propriétaire sans "+"
-  SESSION_ID: "votre-id-sessions~xxx#xxx", // ID de session MEGA
-  BOT_NAME: "Nom De Votre Bot",      // Nom affiché du bot
-  AUTO_READ: true,                   // Lire automatiquement les messages
-  AUTO_TYPING: false,                 // Simuler écriture
-  AUTO_RECORDING: false               // Simuler audio enregistrement
+  PREFIX: "!",
+  OWNER_NUMBER: "241xxxxxxxx",
+  SESSION_ID: "votre-id-sessions~xxx#xxx",
+  BOT_NAME: "Nom De Votre Bot",
+  MODE: "public",
+  SUDO: []
 };
