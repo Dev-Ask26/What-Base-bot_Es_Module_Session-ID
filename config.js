13a65bfbff4259701dc3b6cb79f84e28de2f02a4
Dev Ask <techdevask@gmail.com>
2025-10-01 17:58:55 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// ==================== config.js ====================
+export default {
+  PREFIX: "!",                       // Préfixe des commandes
+  OWNER_NUMBER: "241xxxxxxxx",       // Numéro propriétaire sans "+"
+  SESSION_ID: "votre-id-sessions~xxx#xxx", // ID de session MEGA
+  BOT_NAME: "Nom De Votre Bot",      // Nom affiché du bot
+  AUTO_READ: true,                   // Lire automatiquement les messages
+  AUTO_TYPING: false,                 // Simuler écriture
+  AUTO_RECORDING: false               // Simuler audio enregistrement
+};
