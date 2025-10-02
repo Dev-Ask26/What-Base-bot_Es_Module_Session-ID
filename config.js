937caac27bafe339525599a583845cbb84e56ce0
Dev Ask <techdevask@gmail.com>
2025-10-02 14:59:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 // ==================== config.js ====================
-export default {
-  PREFIX: "!",
-  OWNER_NUMBER: "241xxxxxxxx",
-  SESSION_ID: "votre-id-sessions~xxx#xxx",
+const config = {
+  PREFIX: ",",
+  OWNER_NUMBER: "24165726941",
+  SESSION_ID: "entre votre sessions id",
   BOT_NAME: "Nom De Votre Bot",
   MODE: "public",
   SUDO: []
 };
+
+export default config;
